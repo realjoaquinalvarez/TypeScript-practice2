10b30ea4896b5b740acf64153c23b4a18de56c65
Joaquin <clcflecha@gmail.com>
2024-10-31 23:56:29 -0300

    interfaces

--- src/generics/generics.ts
+++ src/generics/generics.ts
@@ -3,6 +3,8 @@ export const printObject = ( argument: any ) => {
     console.log( argument )
 }
 
-export function genericFunction<T> ( argument: any ) {
+export function genericFunction<T> ( argument: T ) {
     return argument
 }
+
+export const genericFunctionArrow = <T>( argument: T ) => argument
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,22 @@
-import { genericFunction, printObject } from "./generics/generics";
+import { Hero } from "./classes/Hero";
+import { genericFunction, genericFunctionArrow, printObject } from "./generics/generics";
+import { Villain } from "./interfaces";
 
 
-console.log( genericFunction(3.14324).toFixed(2) )
-console.log( genericFunction('Hola Mundo').toFixed(2) )
-console.log( genericFunction( new Date() ).toFixed(2) )
+
+
+
+
+
+// const name: string = 'Fernando'
+// console.log( genericFunctionArrow(3.14324).toFixed(2) )
+// console.log( genericFunctionArrow(name).toUpperCase() )
+// console.log( genericFunctionArrow( new Date() ).getDate() )
+
+const deadpool = {
+    name: 'Deadpool',
+    realName: 'Wade Winston Wilson',
+    dangerLevel: 130
+}
+
+console.log( genericFunctionArrow<Villain>(deadpool) )
